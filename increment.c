432f864f2bc4477ab79f3e5bfdb47401c682a7a2
daniaXmasri <132768928+daniaXmasri@users.noreply.github.com>
2024-12-26 21:18:20 +0300

    Add files via upload

---
+++ increment.c
@@ -0,0 +1,18 @@
+
+/*
+* Grup uyeleri
+* Hanan Haidar, B211210581
+* Dania Almassri, B221210569
+* Tasnim abdulhay, B211210585
+* REEM almadhagi, Y245012049
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int num;
+    if (scanf("%d", &num) == 1) {
+        printf("%d\n", num + 1);
+    }
+    return 0;
+}
